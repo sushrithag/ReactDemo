f3f6d54c1382a2e2259f07e23486152f4f3c6c98
Sushritha Gone <sushrithagone@Sushrithas-MacBook-Air.local>
2024-04-15 20:59:44 -0700

    Added no of clicks to Button.js

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,16 @@
+import {useState} from 'react'
+
 function Button() {
+    
+    const [click_count, setClickCount] = useState(0);
+    
+    function countClicks(){
+        setClickCount(click_count + 1);
+        // alert(click_count);
+    }
+
     return (
-      <button className="button_color">Submit</button>
+      <button onClick={countClicks} className="button_color">No. of clicks are : {click_count}</button>
     );
   }
 
